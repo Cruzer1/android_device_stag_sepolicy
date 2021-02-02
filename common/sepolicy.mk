ed0a94f82f2a5ec1d0f2f5ee9dde73134b9c72b0
Cruzer1 <preetamswain143@gmail.com>
2021-02-02 04:08:15 +0000

    We don't Trust

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -23,8 +23,7 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/stag/sepolicy/common/dynamic \
-    device/stag/sepolicy/common/system
+    device/stag/sepolicy/common/dynamic
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/stag/sepolicy/common/dynamic \
